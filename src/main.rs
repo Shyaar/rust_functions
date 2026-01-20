56133bc28f23cb5613b42066e39f4366a62ac1d8
Shyaar Sati Fwatshak <shyaarsati@gmail.com>
2026-01-21 00:29:09 +0100

    control flow

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,28 @@
+//control flow
+
 fn main(){
 
-    //conditionals flow
+//simple loop
+    // loop{
+    //     println!("this is loop");
+    //     break;
+    // }
 
-    let num = 600;
+    //for loop
 
-    //if else
-    if num < 50 {
-        println!("this number is less than 50 {}", num);
-    } else {
-        println!("this number is greater than 50 {num}")
-    };
+    // let vector: Vec<i32> = vec![1,2,3,4,5];
 
-    // if else if
+    // for i in vector{
+    //     println!("this is i {i}");
+    // }
 
-    fn get_grade(s_score: i8, s_name: &str) -> (&str, char){
+    //while loop
 
-            let grade = if s_score > 90 {
-                'A'
-            } else if s_score >= 70 && s_score <90 {
-                'B'
-            } else if s_score >= 55 && s_score <80 {
-                'C'
-            }else if s_score >=40 && s_score < 55{
-                'D'}else {
-                'F'};
+    let mut num = 0;
 
-                (s_name, grade)
+    while num<=5{
+        num +=1;
+        let num_loop = num;
+        println!("this is i {num_loop}")
     }
-
-    let student_1 = get_grade(50, "john");
-
-    println!("this is student 1s grade {student_1:?}");
-
-
-    let salary = 40;
-    let mut category = 'A';
-
-    match salary {
-        90..=100 => category = 'A',
-        80..=89 => category = 'B',
-        70..=79 => category = 'C',
-        _=> category = 'D'
-    };
-
-    println!("this is category {category}");
-
-    let match_with_tuple = ('a','b');
-
-    let value = match match_with_tuple {
-        ('a', y) =>{ 
-            println!("this has a, second value is {}",y);
-    y
-}
-        (z, 'b') => {println!("this has b, first value is {}",z); z},
-        (a,b) => {println!("this is both values {},{}", a,b); a}   
-    };
-
-    println!{"this is value{value:?}"};
-    
 }
