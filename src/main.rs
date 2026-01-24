a9eeebf707a5e9516a32901f28bc9faaf24b67c8
Shyaar Sati Fwatshak <shyaarsati@gmail.com>
2026-01-24 23:44:30 +0100

    learning operators

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
-
-    // compiler directives
-    // these are used to give instructions to the compiler on how to compile/parse the code
-    // we can use compiler directives directly on a line, on a block scope or on file level
-
-// #[allow(unused_variables)]
-// #[allow(dead_code)]
-// fn name(){
-//     let name = "john";
-// }
-//     fn main(){
-//     #[allow(unused_variables)]
-// let a = 32;
-//     #[allow(unused_variables)]
-// let b =24;
-// }
-
-use std::string;
-
 fn main(){
-    let _x: String = String::from("given");
+    //arithmethic operatos
+    // +_*%/
+    // rust doesnt exponent like other languages using ^
+    // rust uses pow() method for expo
+
+    // comparison operators compares two values and returns a bool
+    // ==, >=, <=, !=, <, >
+    // equality is not same as assignment == & =
+
+    //logical operators
+    // combining multiple bools conditions 
+    // &&, ||, !
+
+
+    // assignment ops are used to assign value to variables
+    // +=, -=, *=, /=, %=
+
+    //bitwise operators 
+    // perform binary operators, directly affecting them on binary level
+    // only used on integer numbers
+
+    //&, |, ^, ~, <<' >>
+
+    let mut x = 4;
+
+    x = x ^ x;
+
+    println!("this is binary x&X:::::::{x}");
+
+    //used mainly for system operation etc
+    
+
+
 }
