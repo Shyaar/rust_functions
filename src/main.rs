754f28f5011d46f18262578632053919eef5d4ef
Shyaar Sati Fwatshak <shyaarsati@gmail.com>
2026-01-25 01:15:39 +0100

    function ownership

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,30 @@
-// ownership in rust 
+
+fn take(vec: Vec<i32>){
+    println!("this is vec {vec:?}");
+}
+
+fn give()-> &str {
+    "name"
+}
 
 fn main(){
-    // in rust, each value has a variable as it owner
-    // there can only be one owner of a (heap) value, thus two variable cannot own the same value;
-    // if the owner goes out of scope, the value is dropped
+    // ownership in functions
 
-    let a =String::from ("given");
-    let b =a;
-    let c = b;
+    //functions that take ownership
+    // when we pass a heap value to a function, it has the same effect as passing a heap value to another variable and the value is moved to the new function
 
-    // here, strings are stored on the heap, when we move owner from a to b, a is no longer accessoble on the heap, be now owns the value give. same goes when the value is moved from b to c
+    let vector: Vec<i32> = vec![1,2,3,4,5];
 
-    // println!("this is a {a}, this is b {b}");
+    // take(vector);
+    // can fix by clone
 
-    // to solve this we can clone or borrow;;
+    take(vector.clone());
+    let c = vector;
 
-    let z =String::from("read");
-    let y = z.clone();
+    // functions that give ownership back
+    let name_of = give()
+    println!("this is name {name_of}")
 
-    // this would clone z into y thus having different allocations of the same value in the heap;; or
-
-    let x = &z;
-
-
-    // values also only live within their scope//
-
-    {
-        let w = z;
-    }
-
-    // let e = w; we get this error cannot find value `w` in this scope:: because after the block scope, w is droped and inaccessible
-
-
-
-  
 
 
 }
---
+++ src/main7.rs
@@ -0,0 +1,49 @@
+// ownership in rust 
+
+fn main(){
+    // in rust, each value has a variable as it owner
+    // there can only be one owner of a (heap) value, thus two variable cannot own the same value;
+    // if the owner goes out of scope, the value is dropped
+
+    let a =String::from ("given");
+    let b =a;
+    let c = b;
+
+    // here, strings are stored on the heap, when we move owner from a to b, a is no longer accessoble on the heap, be now owns the value give. same goes when the value is moved from b to c
+
+    // println!("this is a {a}, this is b {b}");
+
+    // to solve this we can clone or borrow;;
+
+    let z =String::from("read");
+    let y = z.clone();
+
+    // this would clone z into y thus having different allocations of the same value in the heap;; or
+    // this is a deep copy
+
+    let x = &z;
+
+
+    // values also only live within their scope//
+
+    {
+        let w = z;
+    }
+
+    // let e = w; we get this error cannot find value `w` in this scope:: because after the block scope, w is droped and inaccessible
+
+    // in primitive types, since they are stored on the stack, they dont have an ownership error
+
+    let r = 32;
+    let f = r;
+
+    let q = r;
+
+    // here we dont get an error. here, the value is just copied 
+
+
+
+  
+
+
+}
